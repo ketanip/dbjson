2186177b07a162fe17061e9ed0348c864a27b19c
Ketan Iralepatil <ketaniralepatil1@gmail.com>
2021-02-07 17:44:20 +0530

    New Name for Repo

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jsondbb", # Replace with your own username
+    name="dbjson", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
+    author="Ketan Iralepatil",
     author_email="",
     description=SHORT_DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/KetanIP/jsondbb",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
